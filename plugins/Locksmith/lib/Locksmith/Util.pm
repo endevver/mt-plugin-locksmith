ba8b7c0c2976baa390890fa7deb2110379a97af6
Dan Wolfgang <dan@uinnovations.com>
2016-06-02 10:13:09 -0400

    Override MT::Plugin::apply_default_settings using Sub::Install.

--- plugins/Locksmith/lib/Locksmith/Util.pm
+++ plugins/Locksmith/lib/Locksmith/Util.pm
@@ -7,21 +7,32 @@ $Data::Dumper::Maxdepth = 99;
 my $mt_apply_default_settings;
 
 sub init_app {
-    my ($app) = @_;
+    my ($cb, $app) = @_;
 
-    local $SIG{__WARN__} = sub {};
     $mt_apply_default_settings = \&MT::Plugin::apply_default_settings;
-    *MT::Plugin::apply_default_settings = \&apply_default_settings;
+
+    require Sub::Install;
+    Sub::Install::reinstall_sub({
+        code => \&apply_default_settings,
+        into => 'MT::Plugin',
+        as   => 'apply_default_settings'
+    });
 }
 
 sub apply_default_settings {
     my ($plugin, $data, $scope_id) = @_;
-    return &{$mt_apply_default_settings}(@_) unless ($plugin->id eq 'locksmith');
-    return &{$mt_apply_default_settings}(@_) if ($scope_id eq 'system');
+
+    # We only want to override the default settings for Locksmith and at the
+    # blog level.
+    return $mt_apply_default_settings->( @_ )
+        unless $plugin->id eq 'locksmith';
+    return $mt_apply_default_settings->( @_ )
+        if $scope_id eq 'system';
+
     my $sys;
     for my $key (keys %{$plugin->registry('settings')}) {
         next if exists($data->{$key});
-            # don't load system settings unless we need to
+        # don't load system settings unless we need to
         $sys ||= $plugin->get_config_obj('system')->data;
         $data->{$key} = $sys->{$key};
     }
